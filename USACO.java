78dc5055f4cb600b4006a245a259e1de6ab35ccb
Michael Zhang <mzhang00@stuy.edu>
2019-03-06 10:09:54 -0500

    Import necessary files, update bronze problem method

--- USACO.java
+++ USACO.java
@@ -1,9 +1,14 @@
+import java.util.*;
+import java.io.*;
+
 public class USACO{
   private static int R;
   private static int C;
   private static int N;
   private static int E;
-  public static int bronze(String filename){
+  public static int bronze(String filename) throws FileNotFoundException{
+    File text = new File(filename);
+    Scanner inf = new Scanner(text);
     return 1;
   }
 }
