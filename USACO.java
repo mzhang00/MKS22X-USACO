86808830dc28ea757f834e1c46300b9216665f63
Michael Zhang <mzhang00@stuy.edu>
2019-03-06 10:14:13 -0500

    Update code to instantiate instance variables

--- USACO.java
+++ USACO.java
@@ -4,11 +4,17 @@ import java.io.*;
 public class USACO{
   private static int R;
   private static int C;
-  private static int N;
   private static int E;
+  private static int N;
   public static int bronze(String filename) throws FileNotFoundException{
     File text = new File(filename);
     Scanner inf = new Scanner(text);
+    String line = inf.nextLine();
+    String[] values = line.split(" ", -2);
+    R = Integer.parseInt(values[0]);
+    C = Integer.parseInt(values[1]);
+    E = Integer.parseInt(values[2]);
+    N = Integer.parseInt(values[3]);
     return 1;
   }
 }
