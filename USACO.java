acd31224a2a3c40e78bb47b4d8cd2e5975834383
Michael Zhang <mzhang00@stuy.edu>
2019-03-06 10:01:29 -0500

    Add instance variables for USACO

--- USACO.java
+++ USACO.java
@@ -1,4 +1,8 @@
 public class USACO{
+  private static int R;
+  private static int C;
+  private static int N;
+  private static int E;
   public static int bronze(String filename){
     return 1;
   }
